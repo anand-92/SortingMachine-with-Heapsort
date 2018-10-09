1887366156e9ee8726bf7d500dad1b9e18e92408
harledge <42972091+harledge@users.noreply.github.com>
2018-10-09 14:03:54 -0400

    Update SortingMachineTest.java

--- SortingMachineTest.java
+++ SortingMachineTest.java
@@ -10,7 +10,7 @@ import components.sortingmachine.SortingMachine;
  * JUnit test fixture for {@code SortingMachine<String>}'s constructor and
  * kernel methods.
  *
- * @author Put your name here
+ * @author Nik Anand and Hudson Arledge
  *
  */
 public abstract class SortingMachineTest {
